040301966e2612ceccdcbbadb611a74c63525b5e
argun8394 <argundoganali@gmail.com>
2022-02-08 01:06:00 +0300

    styling_components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/styled-comp/button/Button.styles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Styled Components & Material-UI</h1>
+     <Button>Click me</Button>
     </div>
   );
 }
---
+++ src/components/styled-comp/button/Button.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+border:none;
+margin:2px;
+color:#fff;
+padding:10px;
+`;
