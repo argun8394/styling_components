9cb5a51bf7556d37feebd5c1905e8652a44f4619
argun8394 <argundoganali@gmail.com>
2022-02-09 22:30:29 +0300

    create styled Header_comp

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {Button} from "../styled-comp/button/Button.styles";
-import Wrapper from "./wrapper/Wrapper"
+import Wrapper from "./wrapper/Wrapper";
+import HeaderText from "./header/HeaderText";
 
  const StyledComponents = ()=>{
     return(
         <Wrapper /*row*/>
+           <HeaderText>Clarusway</HeaderText>
+           <HeaderText headerColor="red">header with props</HeaderText>
             <Button>Default</Button>
             <Button primary>Primary</Button>
         </Wrapper>
