94bd87e535ba94e8e7600ee5774ab92e61e492b7
argun8394 <argundoganali@gmail.com>
2022-02-09 23:42:15 +0300

    extended link comp

---
+++ src/components/styled-comp/extended-link/ExtendedComp.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Link from "../link/Link";/*Link i extend ederek kullanım şekli */
+
+export default styled(Link)`/*Extend ettiğimiz componeneti parametre olarak gönderdik */
+background-color: violet;
+margin-top: 1rem;
+margin-bottom: 1rem;
+`;
--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -3,6 +3,7 @@ import {Button} from "../styled-comp/button/Button.styles";
 import Wrapper from "./wrapper/Wrapper";
 import HeaderText from "./header/HeaderText";
 import Link from "./link/Link";
+import ExtendedComp from "./extended-link/ExtendedComp";
 
  const StyledComponents = ()=>{
     return(
@@ -10,6 +11,7 @@ import Link from "./link/Link";
            <HeaderText>Clarusway</HeaderText>
            <HeaderText headerColor="red">Header with props</HeaderText>
            <Link secondary href="https://clarusway.com/" target="_blank" rel="noopener">Learn FS development</Link>
+           <ExtendedComp>extended from Link comp</ExtendedComp>
             <Button>Default</Button>
             <Button primary>Primary</Button>
         </Wrapper>
