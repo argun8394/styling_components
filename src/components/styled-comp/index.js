bf30f9e2c552aa74ea7ab900452370d8af233825
argun8394 <argundoganali@gmail.com>
2022-02-09 23:33:38 +0300

    create link comp

--- src/components/styled-comp/index.js
+++ src/components/styled-comp/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import {Button} from "../styled-comp/button/Button.styles";
 import Wrapper from "./wrapper/Wrapper";
 import HeaderText from "./header/HeaderText";
+import Link from "./link/Link";
 
  const StyledComponents = ()=>{
     return(
         <Wrapper /*row*/>
            <HeaderText>Clarusway</HeaderText>
-           <HeaderText headerColor="red">header with props</HeaderText>
+           <HeaderText headerColor="red">Header with props</HeaderText>
+           <Link secondary href="https://clarusway.com/" target="_blank" rel="noopener">Learn FS development</Link>
             <Button>Default</Button>
             <Button primary>Primary</Button>
         </Wrapper>
