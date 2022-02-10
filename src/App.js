948d05794df38cd5a43f09e3518474b7e42db897
argun8394 <argundoganali@gmail.com>
2022-02-10 15:19:46 +0300

    create input_(textfieldcomp)

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import './App.css';
 // import StyledComponents from './components/styled-comp/index';
 import Buttons from './components/material-ui/buttons/Buttons';
 import CheckBoxComp from './components/material-ui/checkbox/CheckBoxComp';
+import TextFieldComp from './components/material-ui/text-field/TextFieldComp';
+import {ThemeProvider,createMuiTheme} from '@material-ui/core/styles';
+import { orange, green } from '@material-ui/core/colors';
 
+const theme = createMuiTheme({ /*Tema değişikliklerini gönderdik */
+  palette: {
+    primary: {
+      main:orange[400]
+    },
+    secondary: {
+      main: green[300]
+    }
+  }
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>{/*Tema değişikliği yapmamız için ana componenti themeProvider ile sarmallayarak uyguladık */}
     <div className="App">
       <header className="App-header">
      {/* <h1>Styled Components & Material-UI</h1> */}
      {/* <Button>Click me</Button>
      <Button primary>Second</Button> */}
      {/*<StyledComponents/>{/*App.js kalabalık olmaması açısından yukarıdaki kullanım yerine bu kullanım tercih edilmelidir */}
-     
+     <TextFieldComp/>
      <CheckBoxComp/>
      <Buttons/>
 
      
      </header>
     </div>
+    </ThemeProvider>
   );
 }
 
