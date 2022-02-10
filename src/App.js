e5e2e636fbce227e448835edcec88adb0b330d76
argun8394 <argundoganali@gmail.com>
2022-02-10 16:22:25 +0300

    m-ui container using

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import './App.css';
 import Buttons from './components/material-ui/buttons/Buttons';
 import CheckBoxComp from './components/material-ui/checkbox/CheckBoxComp';
 import TextFieldComp from './components/material-ui/text-field/TextFieldComp';
-import {ThemeProvider, createTheme} from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 import { orange, green } from '@material-ui/core/colors';
 import TypographyComp from './components/material-ui/typography/TypographyComp';
+import Wrapper from './components/material-ui/wrapper/Wrapper'
 
 const theme = createTheme({ /*Tema değişikliklerini gönderdik */
   palette: {
     primary: {
-      main:orange[400]
+      main: orange[400]
     },
     secondary: {
       main: green[300]
     },
     typography: {/*Typography nin stillemesini istediğimiz şekilde */
-      h2: {fontSize:38,
-      padding:2},
+      h2: {
+        fontSize: 38,
+        padding: 2
+      },
       subtitle1: {
         marginBottom: 10
       }
@@ -30,20 +33,22 @@ const theme = createTheme({ /*Tema değişikliklerini gönderdik */
 function App() {
   return (
     <ThemeProvider theme={theme}>{/*Tema değişikliği yapmamız için ana componenti themeProvider ile sarmallayarak uyguladık */}
-    <div className="App">
-      <header className="App-header">
-     {/* <h1>Styled Components & Material-UI</h1> */}
-     {/* <Button>Click me</Button>
+      <Wrapper>
+        <div className="App">
+          <header className="App-header">
+            {/* <h1>Styled Components & Material-UI</h1> */}
+            {/* <Button>Click me</Button>
      <Button primary>Second</Button> */}
-     {/*<StyledComponents/>{/*App.js kalabalık olmaması açısından yukarıdaki kullanım yerine bu kullanım tercih edilmelidir */}
-     <TypographyComp/>
-     <TextFieldComp/>
-     <CheckBoxComp/>
-     <Buttons/>
+            {/*<StyledComponents/>{/*App.js kalabalık olmaması açısından yukarıdaki kullanım yerine bu kullanım tercih edilmelidir */}
+            <TypographyComp />
+            <TextFieldComp />
+            <CheckBoxComp />
+            <Buttons />
 
-     
-     </header>
-    </div>
+
+          </header>
+        </div>
+      </Wrapper>
     </ThemeProvider>
   );
 }
